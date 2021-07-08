1e50cfe2842965f9a3d707e8f849251f68603085
Masoud Omidvar <masoudomidvar7@gmail.com>
2021-07-08 13:58:32 +0430

    fix array_search problem in edit employee services

--- application/views/employee/edit.php
+++ application/views/employee/edit.php
@@ -162,7 +162,6 @@
 
                             <div class="col-sm-5">
                                 <select name="service[]" class="select-box form-control margin-bottom"  multiple="multiple">
-
                                     <?php
                                     foreach ($services as $row) {
                                         $cid = $row->id;
@@ -172,7 +171,8 @@
                                             $cidx = $rowx->id;
                                             $titlex = $rowx->name;
                                             $service_pieces = explode(",", $user['service']);
-                                            if( array_search($cidx,$service_pieces )){
+                                            // if( array_search($cidx,$service_pieces )){
+                                            if( array_search($cidx, $service_pieces) !== false){
                                                 echo ' <option selected value="' . $cidx . '"> ' . $titlex . '</option>';
                                             }
                                             else{
